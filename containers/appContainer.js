befd381a1a515c3819e400e60f361d7e93da5174
Adam Finch <atyreefinch@gmail.com>
2019-12-18 11:48:15 -0500

    i think nav basically done

--- containers/appContainer.js
+++ containers/appContainer.js
@@ -3,34 +3,85 @@ import GameContainer from './gameContainer'
 import {StatusBar} from 'react-native'
 import {connect} from 'react-redux'
 import VowelGameContainer from './vowelGameContainer'
-import {createAppContainer} from 'react-navigation'
-import {createStackNavigator} from 'react-navigation-stack'
 import { MaterialIcons } from '@expo/vector-icons';
+import { Audio } from 'expo-av'
+import paths from '../assets/wordsJson'
 
 const Fragment=React.Fragment
 
+let _this = null
 
 class GamesContainer extends React.Component {
 
-
   static navigationOptions = ({ navigation }) => ({
     headerLeft: <MaterialIcons name="home" size={32} color="white" onPress={ () => { navigation.goBack() } } style={{left:10}}  />,
-    
+    headerRight: <MaterialIcons name="live-help" size={32} color="white" onPress={()=> _this.saySomething() } style={{right:10}}  />,
     headerStyle: {
         backgroundColor: '#228b22',
       },
       headerTintColor: 'white',
   });
 
+async saySomething() {
+  console.log("sup")
+  const source3 = paths()[`${this.props.selectedWord}`];
+  const initialStatus = {
+    //        Play by default
+              shouldPlay: true,
+    //        Control the speed
+              rate: 1.0,
+    //        Correct the pitch
+              shouldCorrectPitch: true,
+    //        Control the Volume
+              volume: 1.0,
+    //        mute the Audio
+              isMuted: false
+         };
+  
+         const { sound, status } = await Audio.Sound.createAsync(
+             source3,
+             initialStatus
+        );
+  
+    //  Save the response of sound in playbackInstance
+          
+        this.wordSound = sound;
+        
+  
+    //  Play the Music
+   
+        sound.playAsync();
+      setTimeout(()=>this.unload(sound),2500)
+  
+}
+
+unload = (sound) => {
+  sound.unloadAsync()
+}
+
+
+componentDidMount = () => {
+  Audio.setAudioModeAsync({
+    allowsRecordingIOS: false,
+    interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+    playsInSilentModeIOS: true,
+    shouldDuckAndroid: true,
+    interruptionModeAndroid:          Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+    playThroughEarpieceAndroid: false,
+});
+  _this = this
+}
+
 render() { return <Fragment><StatusBar barStyle="light-content" backgroundColor="red" />
 {this.props.level < 7 ? <GameContainer/> : <VowelGameContainer/>}</Fragment>}
 }
 
-mapDispatchToProps = (state) => {
-    return {level: state.level}
+mapStateToProps = (state) => {
+    return {level: state.level,
+            selectedWord: state.selectedWord}
 }
 
-export default connect(mapDispatchToProps)(GamesContainer)
+export default connect(mapStateToProps)(GamesContainer)
 
 
 // 
